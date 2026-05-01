33252f03974946d064f308ffb96a68a86bfacc0f
Éderson <d3rson.br@gmail.com>
2026-05-01 19:54:43 -0300

    adicionando normalizador HTML para evitar tags aninhadas e vazias

---
+++ src/plugins/normalizer.js
@@ -0,0 +1,56 @@
+function NormalizerPlugin(editor) {
+  this.editor = editor;
+}
+
+NormalizerPlugin.prototype.init = function () {
+  this.bind();
+};
+
+NormalizerPlugin.prototype.bind = function () {
+  var self = this;
+
+  this.editor.$content.on('blur', function () {
+    self.normalize();
+  });
+};
+
+NormalizerPlugin.prototype.normalize = function () {
+  var root = this.editor.$content[0];
+
+  this.flatten(root, 'b');
+  this.flatten(root, 'i');
+  this.removeEmpty(root);
+};
+
+NormalizerPlugin.prototype.flatten = function (root, tag) {
+  var nodes = root.querySelectorAll(tag + ' ' + tag);
+
+  for (var i = 0; i < nodes.length; i++) {
+    var inner = nodes[i];
+    var parent = inner.parentNode;
+
+    while (inner.firstChild) {
+      parent.insertBefore(inner.firstChild, inner);
+    }
+
+    parent.removeChild(inner);
+  }
+};
+
+NormalizerPlugin.prototype.removeEmpty = function (root) {
+  var nodes = root.querySelectorAll('*');
+
+  for (var i = nodes.length - 1; i >= 0; i--) {
+    var el = nodes[i];
+
+    if (
+      el.childNodes.length === 0 &&
+      el.tagName !== 'BR' &&
+      el.textContent.trim() === ''
+    ) {
+      el.parentNode.removeChild(el);
+    }
+  }
+};
+
+window.NormalizerPlugin = NormalizerPlugin;
